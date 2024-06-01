dc2865b81fd491be6edc645900118d1030804872
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-06-01 13:40:48 -0700

    Created submission counter and Chris's version of counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// ASSIGNMENT COUNTER STARTS HERE
+
+// import React from 'react'
+// import { useState } from 'react'
+// How to write this code in one line --> import React, { useState } from 'react'
+
+// function App() {
+//   let [ counter, changeCounter ] = useState(1)
+//   window.changeCounter = changeCounter
+//   return (
+//     <div>
+//       <h1>{counter}</h1>
+//       <h4>{counter}</h4>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ASSIGNMENT COUNTER ENDS HERE
+
+// CHRIS'S VERSION OF COUNTER STARTS HERE
+
+import {useState} from 'react'
+import './App.css'
 
 function App() {
+  const [ counter, setCounter ] = useState(0)
+
+  const handleIncrement = () => {
+    setCounter(counter + 1)
+  }
+
+  const handleDecrement = () => {
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Cool Counter</h1>
+      <h2>Current Count is: {counter} </h2>
+
+      <button onClick={handleIncrement}>+1</button>
+      <button onClick={handleDecrement} disabled={counter <=0 }>-1</button>
     </div>
   );
 }
 
 export default App;
+
+//CHRIS'S VERSION OF COUNTER ENDS HERE
